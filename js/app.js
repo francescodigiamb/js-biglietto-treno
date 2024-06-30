683b0f9c13eb89f08a94c2fd5773f0e2a3c93872
francescodgiamb <francescodigiamb@gmail.com>
2024-06-30 12:52:10 +0200

    ADD:manca-bonus

--- js/app.js
+++ js/app.js
@@ -1,1 +1,44 @@
-console.log('ciao mondo')
+// console.log('ciao mondo')
+
+// Chiedere all'utente il numero di chilometri da percorrere
+
+let km = prompt ('Quanti chilometri devi percorrere');
+console.log (km)
+
+// Chiedere all'utente l'età
+
+let età = prompt ('Quanti anni hai')
+console.log (età)
+
+// Calcolare il prezzo totale del viaggio
+    // prezzo del biglietto è definito in base ai km (0.21€ al km)
+    let prezzoBiglietto = 0.21 * km
+    console.log (prezzoBiglietto.toFixed(2))
+    // va applicato uno sconto del 20% per i minorenni
+
+    let prezzoScontato;
+
+    if (età < 18) {
+
+        prezzoScontato = prezzoBiglietto - ( prezzoBiglietto * 0.20 )
+        
+        console.log (prezzoScontato.toFixed(2))
+
+        // va applicato uno sconto del 40% per gli over 65
+    } else if (età > 65) {
+        
+        prezzoScontato = prezzoBiglietto - ( prezzoBiglietto * 0.40)
+    
+        console.log (prezzoScontato.toFixed(2))
+    } else {
+
+        console.log (prezzoBiglietto.toFixed(2))
+        
+    }
+    
+    // Stampare il prezzo del biglietto in console
+
+
+
+
+// Output del prezzo finale va messo fuori in forma umana con massimo due decimali
